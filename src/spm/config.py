85024ca61868e1cebe60497180ecaac640fbe67a
m-hosein-dk <darvazehkashani@gmail.com>
2026-01-07 19:17:21 +0330

    changes to configs

--- src/spm/config.py
+++ src/spm/config.py
@@ -1,9 +1,21 @@
 import os
 
-DATABASE_URL = os.environ.get("SPM_DATABASE_URL")
+def env(name:str, default:str | None = None):
+    if default is not None:
+        return os.environ.get(name, default)
+    else:
+        env = os.environ.get(name)
 
-JWT_SECRET = os.environ.get("JWT_SECRET")
-JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM", "HS256")
+        if not env:
+            raise ValueError('"DATABASE_URL" needs to be set as an environment variable')
+        
+        return env
 
-DEFAULT_USERNAME = os.environ.get("DEFAULT_USERNAME")
-DEFAULT_PASSWORD = os.environ.get("DEFAULT_PASSWORD")
+
+DATABASE_URL = env("SPM_DATABASE_URL")
+
+JWT_SECRET = env("JWT_SECRET")
+JWT_ALGORITHM = env("JWT_ALGORITHM", "HS256")
+
+DEFAULT_USERNAME = env("DEFAULT_USERNAME")
+DEFAULT_PASSWORD = env("DEFAULT_PASSWORD")
