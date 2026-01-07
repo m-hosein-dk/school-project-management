3346407d19abd5fd34b16f13c0681324412b44e6
m-hosein-dk <darvazehkashani@gmail.com>
2026-01-07 23:19:32 +0330

    changed permission bases

--- src/spm/permissions.py
+++ src/spm/permissions.py
@@ -3,27 +3,23 @@ from abc import ABC, abstractmethod
 from fastapi import Request
 from fastapi.exceptions import HTTPException
 
+def any_permission(permissions: list[type["BasePermissions"]], request:Request) -> bool:
+	for permission in permissions:
+		if permission().check(request):
+			return True
+	return False
+
 class BasePermissions(ABC):
-	bases: list["BasePermissions"] = []
 	@abstractmethod
 	def check(self, request: Request) -> bool: ...
 
 class PermissionsDependency:
 	def __init__(self, permissions: list[type[BasePermissions]]) -> None:
 		self.permissions = permissions
-	
-	def check_primission(self, permission: type[BasePermissions], request:Request):
-		permission_instence = permission()
-		base_check = any([
-			p.check(request)
-			for p in permission_instence.bases
-		])
-
-		return any([permission_instence.check(request), base_check])
 
 	def __call__(self, request:Request) -> Any:
 		check_result = any([
-			self.check_primission(p, request)
+			p().check(request)
 			for p in self.permissions
 		])
 
